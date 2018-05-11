bd190569c52d96c063d253f6d67def50ea00d1ba
salody <salodyliu@gmail.com>
2018-05-11 17:40:25 +0800

    add ignore file

--- data-structure/SinglyList.js
+++ data-structure/SinglyList.js
@@ -2,6 +2,7 @@
  * 单链表
  *  https://codeburst.io/js-data-structures-linked-list-3ed4d63e6571
  *  https://code.tutsplus.com/articles/data-structures-with-javascript-singly-linked-list-and-doubly-linked-list--cms-23392
+ *  https://juejin.im/post/5aa299c1518825557b4c5806
  */
 
 class Node {
