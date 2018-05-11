5216d010128f6fa5ecaf8346afb19c771fc3c2a9
salody <salodyliu@gmail.com>
2018-05-11 17:22:09 +0800

    git init

--- data-structure/SinglyList.js
+++ data-structure/SinglyList.js
@@ -1,6 +1,7 @@
 /**
  * 单链表
- *
+ *  https://codeburst.io/js-data-structures-linked-list-3ed4d63e6571
+ *  https://code.tutsplus.com/articles/data-structures-with-javascript-singly-linked-list-and-doubly-linked-list--cms-23392
  */
 
 class Node {
