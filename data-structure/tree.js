ff0e6e73947a2d4af09e1044b6ae6c8b26256917
salody <salodyliu@gmail.com>
2018-05-24 15:23:08 +0800

    add tree

---
+++ data-structure/tree.js
@@ -0,0 +1,8 @@
+/**
+ * Defination
+ * 
+ * A tree is a collection of entities called nodes. 
+ * Nodes are connected by edges.
+ * Each node contains a value or data, 
+ * and it may or may not have a child node .
+ */
